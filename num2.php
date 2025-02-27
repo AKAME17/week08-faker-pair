a6a5564a9e15c89717dec85b52891d94a8441787
Maria Rena Bandiala <202280052@psu.palawan.edu.ph>
2025-02-27 15:33:22 +0800

    Changed Code

--- num2.php
+++ num2.php
@@ -1,24 +1,30 @@
-
 <?php
-require 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 $faker = Faker\Factory::create();
+$genres = ["Fiction", "Mystery", "Science Fiction", "Fantasy", "Romance", "Thriller", "Historical", "Horror"];
 
-$genres = ['Fiction', 'Science Fiction', 'Mystery', 'Romance', 'Thriller', 'Historical'];
+echo "<h2>Fake Books</h2>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Genre</th>
+        <th>Publication Year</th>
+        <th>ISBN</th>
+        <th>Summary</th>
+      </tr>";
 
-echo "<h2>Fake Book Records</h2>";
-echo "<table border='1'>
-        <tr><th>Title</th><th>Author</th><th>Genre</th><th>ISBN</th><th>Publication Year</th><th>Summary</th></tr>";
-
-for ($i = 0; $i < 10; $i++) {
-    echo "<tr>
-            <td>{$faker->sentence(3)}</td>
-            <td>{$faker->name}</td> <!-- Random author name -->
-            <td>{$genres[array_rand($genres)]}</td>
-            <td>{$faker->isbn13}</td>
-            <td>{$faker->numberBetween(1900, 2024)}</td>
-            <td>{$faker->paragraph(2)}</td> <!-- Random book summary -->
-          </tr>";
+for ($i = 0; $i < 15; $i++) {
+    echo "<tr>";
+    echo "<td>" . $faker->sentence(3) . "</td>"; 
+    echo "<td>" . $faker->name . "</td>";
+    echo "<td>" . $genres[array_rand($genres)] . "</td>";
+    echo "<td>" . $faker->numberBetween(1900, 2024) . "</td>";
+    echo "<td>" . $faker->isbn13 . "</td>";
+    echo "<td>" . $faker->paragraph(1) . "</td>";
+    echo "</tr>";
 }
+
 echo "</table>";
 ?>
